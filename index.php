633fc67d1e13a321a469cdc5aac23b943ed34842
Timur Gaziiev <trilochan.sbbbm@gmail.com>
2015-01-03 19:37:35 +0200

    my first attempt.

---
+++ index.php
@@ -0,0 +1,29 @@
+<p>Задание 2</p>
+<?
+//zadanie 2
+define('CITY', 'Evpatoria');
+var_dump(CITY);
+if (CITY) {
+    echo 'Мой город - '.CITY.'!';
+} 
+//CITY = Mayapur; zdes ya pitaus pomenyat konstantu
+?>
+<br>
+<p>Задание 3</p>
+<?
+//zadanie 3
+$book=array('title'=>'za predeli vaikunthi', 'autor'=>'Sanatana Goswami',
+    'page'=>'100');
+echo 'Недавно я прочитал книгу '.$book['title'].', написанную автором '.$book['autor'].', я осилил
+все '.$book['page'].' страниц, мне она очень понравилась.';
+?>
+<br>
+<p>Задание 4</p>
+<?
+//zadanie 4
+$books=array();
+//$books[]= 
+//$books[]= 
+
+
+?>
