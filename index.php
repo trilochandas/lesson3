3315e826affbdc41d3f9107cfd92eef371e19830
Timur Gaziiev <trilochan.sbbbm@gmail.com>
2015-01-04 23:11:46 +0200

    to check

--- index.php
+++ index.php
@@ -4,31 +4,30 @@
 define('CITY', 'Evpatoria');
 var_dump(CITY);
 if (CITY) {
-    echo 'Мой город - '.CITY.'!';
-} 
+    echo 'Мой город - ' . CITY . '!';
+}
 //CITY = Mayapur; zdes ya pitaus pomenyat konstantu
 ?>
 <br>
 <p>Задание 3</p>
 <?
 //zadanie 3
-$book=array('title'=>'za predeli vaikunthi', 'autor'=>'Sanatana Goswami',
-    'page'=>'100');
-echo 'Недавно я прочитал книгу '.$book['title'].', написанную автором '.$book['autor'].', я осилил
-все '.$book['page'].' страниц, мне она очень понравилась.';
+$book = array('title' => 'za predeli vaikunthi', 'autor' => 'Sanatana Goswami',
+    'page' => '100');
+echo 'Недавно я прочитал книгу ' . $book['title'] . ', написанную автором ' . $book['autor'] . ', я осилил
+все ' . $book['page'] . ' страниц, мне она очень понравилась.';
 ?>
 <br>
 <p>Задание 4</p>
 <?
 //zadanie 4
-$book1=array('title'=>'za predeli vaikunthi', 'autor'=>'Sanatana Goswami',
-    'page'=>'100');
-$book2=array('title'=>'sharanagati', 'autor'=>'Thakur Bhaktivinod',
-    'page'=>'40');
+$arr = array(
+    array('title'  => 'Za predeli vaikunthi', 'author' => 'Sanatana Goswami','pages'=>'100'), 
+    array('title'=>'sharanagati', 'author' =>'Bhaktivinod Thakur','pages'=>'40'),
+);
 
-$books=array($book1,$book2);
-
-var_dump($books);
-
-echo 'я прочитал книгу'.$books[0];
+ echo 'Недавно я прочитал книги ' .$arr[0]['title']. ' и ' .$arr[1]['title'].
+         ', написанные соответственно авторами ' .$arr[0]['author']. 
+    ' и ' .$arr[1]['author'].', я осилил в сумме ' .($arr[0]['pages']+$arr[1]['pages']). 
+        ' страниц, не ожидал от себя подобного.';
 ?>
