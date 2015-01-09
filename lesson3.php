fe6dbcf478b72af49f7d2c0d57723b7d6d93911a
Timur Gaziiev <trilochan.sbbbm@gmail.com>
2015-01-09 10:26:33 +0200

    my second attempt for 3 lesson

--- lesson3.php
+++ lesson3.php
@@ -9,35 +9,64 @@ $a = array(
 );
 
 print_r($a);
+// This is the bad practice =)
+// $_0 = date('N l', $a[0]);
+// $_1 = date('N l', $a[1]);
+// $_2 = date('N l', $a[2]);
+// $_3 = date('N l', $a[3]);
+// $_4 = date('N l', $a[4]);
+?>
+<br>
+<?
 
-$_0 = date('N l', $a[0]);
-$_1 = date('N l', $a[1]);
-$_2 = date('N l', $a[2]);
-$_3 = date('N l', $a[3]);
-$_4 = date('N l', $a[4]);
+$dataDay = array(date('N l', $a[0]), date('N l', $a[1]), date('N l', $a[2]), date('N l', $a[3]), date('N l', $a[4]));
 
 echo "\n  в сгенерированном массиве наименьшим днем получился  : ";
-echo min($_0, $_1, $_2, $_3, $_4);
-
-$_0 = date('m F', $a[0]);
-$_1 = date('m F', $a[1]);
-$_2 = date('m F', $a[2]);
-$_3 = date('m F', $a[3]);
-$_4 = date('m F', $a[4]);
+// This is the bad practice =)
+// echo min($_0, $_1, $_2, $_3, $_4);
+echo min($dataDay[0], $dataDay[1], $dataDay[2], $dataDay[3], $dataDay[4]);
+// This is the bad practice =)
+// $_0 = date('m F', $a[0]);
+// $_1 = date('m F', $a[1]);
+// $_2 = date('m F', $a[2]);
+// $_3 = date('m F', $a[3]);
+// $_4 = date('m F', $a[4]);
+?>
+<br>
+<?
+$dataMonth = array(date('m F', $a[0]), date('m F', $a[1]), date('m F', $a[2]), date('m F', $a[3]), date('m F', $a[4]));
 
 echo "\n  в сгенерированном массиве наибольшим месяцем получился  : ";
-echo min($_0, $_1, $_2, $_3, $_4);
+// This is the bad practice =)
+// echo min($_0, $_1, $_2, $_3, $_4);
+echo min($dataMonth[0], $dataMonth[1], $dataMonth[2], $dataMonth[3], $dataMonth[4]);
+
+?>
+<br>
+<?
 
 sort($a);
 echo "\n Отсортированный массив по возрастанию :\n";
+?>
+<br>
+<?
 print_r($a);
+?>
+<br>
+<?
 
 echo "\n С помощью функция для работы с массивами извлеките последний элемент массива в новую переменную \$selected C помощью функции date() выведите \$selected на экран в формате дд.мм.ГГ ЧЧ:ММ:СС: ";
 $selected = array_pop($a);
 echo date('d.m.Y H:i:s', $selected);
-
+?>
+<br>
+<?
 echo "\n Моя часовая зона до изменения: ";
 echo date_default_timezone_get();
+
+?>
+<br>
+<?
 
 echo "\n Проверка скрипта для себя))";
 $script_tz = date_default_timezone_get();
@@ -47,11 +76,18 @@ if (strcmp($script_tz, ini_get('date.timezone'))){
     echo 'Временные зоны скрипта и настройки INI-файла совпадают.';
 }
 
+?>
+<br>
+<?
+
 echo "\n";
 date_default_timezone_set('America/New_York');
 echo "\n Моя часовая зона после изменения: ";
 echo date_default_timezone_get();
 echo "\n";
+?>
+<br>
+<?
 $script_tz = date_default_timezone_get();
 if (strcmp($script_tz, ini_get('date.timezone'))){
     echo 'Временная зона скрипта отличается от заданной в INI-файле.';
